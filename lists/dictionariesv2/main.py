949d69f83653141bcb429eb9d4f70229094a3e6d
JohnPiters <j.piters@outlook.com>
2026-05-15 15:14:17 +0200

    opdracht

--- lists/dictionariesv2/main.py
+++ lists/dictionariesv2/main.py
@@ -1,42 +1,10 @@
 # Do not modify these lines
-__winc_id__ = '6eb355e1a60f48a28a0bbbd0c88d9ab4'
-__human_name__ = 'lists'
+from helpers import get_countries
+
+__winc_id__ = "00a4ab32f1024f5da525307a1959958e"
+__human_name__ = "dictionariesv2"
 
 # Add your code after this line
-# List of film names
-film_names = [
-    "Star Wars",
-    "Jurassic Park",
-    "Harry Potter",
-    "E.T.",
-    "Indiana Jones"
-]
-
-# Function to sort films alphabetically
-def alphabetical_order(films):
-    return sorted(films)
-
-# Test the function
-print(alphabetical_order(film_names))
-# List of Golden Globe winning films
-golden_globes = [
-    "jaws",
-    "e.t.",
-    "schindler's list",
-    "star wars"
-]
-
-# Function to check if a film won a Golden Globe
-def won_golden_globe(film_name):
-    film_name = film_name.lower()
-    return film_name in golden_globes
-
-
-# Test examples
-print(won_golden_globe("Jaws"))          # True
-print(won_golden_globe("Harry Potter")) # False
-print(won_golden_globe("STAR WARS"))    # True
-
 def create_passport(
     name,
     date_of_birth,
@@ -110,7 +78,6 @@ print(add_stamp(passport, "Spain"))
 print(add_stamp(passport, "France"))
 print(add_stamp(passport, "Spain"))       # duplicate, won't be added
 print(add_stamp(passport, "Netherlands")) # home country, won't be added
-
 def add_biometric_data(passport, name, value, date):
 
     # Create biometric dictionary if it does not exist yet
